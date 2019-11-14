23ff62ae503f0d1612923364fac28e5b639ffa83
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2019-11-14 02:14:39 -0300

    Criação de rota para atualizar informações do user.

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -18,6 +18,34 @@ class UserController {
       email,
     });
   }
+
+  async update(request, response) {
+    const { email, oldPassword } = request.body;
+
+    const user = await User.findByPk(request.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return response.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return response.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name } = await user.update(request.body);
+
+    return response.json({
+      id,
+      name,
+      email,
+    });
+  }
 }
 
 export default new UserController();
