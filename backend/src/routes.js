a065140ed24d4bf750a6ef2f9e2f6e241368dbcc
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2019-11-14 01:09:59 -0300

    Criando um loader de models

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello World' })
-);
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    name: 'Leonardo Pinheiro',
+    email: 'leo.pinnheiro@gmail.com',
+    password_hash: '123',
+  });
+  return response.json(user);
+});
 
 export default routes;
